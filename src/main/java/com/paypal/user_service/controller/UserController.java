672cffea7fd6b37bb0c5c13beb182d72dc10c594
Vikas Dagur <vikasda@interrait.com>
2025-08-11 12:15:56 +0530

    user service completed

--- src/main/java/com/paypal/user_service/controller/UserController.java
+++ src/main/java/com/paypal/user_service/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.paypal.user_service.controller;
 
+import com.paypal.user_service.entity.User;
 import com.paypal.user_service.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users/")
@@ -21,4 +19,15 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user){
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return userService.getUserById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<User> getAllUser(){
+        return ResponseEntity.ok((User) userService.getAllUsers())
+    }
+
 }
--- src/main/java/com/paypal/user_service/repository/UserRepository.java
+++ src/main/java/com/paypal/user_service/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.paypal.user_service.repository;
 
 import com.paypal.user_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository {
+@Repository
+
+public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 
 
